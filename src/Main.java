fa7fb543fb1266a84ed38293022e4826ea269848
Chase Gould <chase.gould7@gmail.com>
2019-04-22 22:15:36 -0700

    refactored main class

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,36 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		//lambda expressions
+		//lambda expressions for color generating equations
 		MyEquation equation1 = (int x) -> { return (int) (1 + (Math.floor(x*Math.PI) % 30)); };
 		MyEquation equation2 = (int x) -> { return (int) (1 + (Math.floor(x*Math.E) % 30)); };		
 		MyEquation equation3 = (int x) -> { return (int) (1 + (Math.floor(x*Math.sqrt(3)) % 30)); };	
 		MyEquation equation4 = (int x) -> { return (int) (1 + (Math.floor(x*Math.sqrt(5)) % 30)); };	
 		
+		//total number of cube faces
 		int n = 180;
 		
+		
 		ArrayList<Cube> cubesList1 = new ArrayList<Cube>();
+		//puzzle generator populates a list of cubes with colors
 		puzzleGenerator(equation1, n, cubesList1);
 		
 		ArrayList<Cube> cubesList2 = new ArrayList<Cube>();
+		//puzzle generator populates a list of cubes with colors
 		puzzleGenerator(equation2, n, cubesList2);
 		
 		ArrayList<Cube> cubesList3 = new ArrayList<Cube>();
+		//puzzle generator populates a list of cubes with colors
 		puzzleGenerator(equation3, n, cubesList3);
 		
 		ArrayList<Cube> cubesList4 = new ArrayList<Cube>();
+		//puzzle generator populates a list of cubes with colors
 		puzzleGenerator(equation4, n, cubesList4);
 		
-		int counter = 0;
-		for(Cube e : cubesList4) {
-			System.out.println("Cube " + counter + ": " + e.toString());
-			counter++;
-		}
+		
+		//returns a string of cube list color assignments. used for testing purposes.
+		String listAssignments = cubesListToString(cubesList1);
+		System.out.println(listAssignments);
 		
 	}
 	
@@ -44,6 +49,17 @@ public class Main {
 			Cube cubey = new Cube(generatedColors);
 			cubesList.add(cubey);
 		}
+	}
+	
+	//testing function outputs all colors from a list of cubes.
+	public static String cubesListToString(ArrayList<Cube> cubesList) {
+		String output = "";
+		int counter = 0;
+		for(Cube e : cubesList) {
+			output += "Cube " + counter + ": " + e.toString() + "\n";
+			counter++;
+		}
+		return output;
 	}
 	
 }
